7f32c53e95f0bd4e40a6c7b00cd0f28f25c49a20
Adam Mihalik <adam.mihalik@inloop.eu>
2016-12-06 10:02:49 +0100

    Forgotten sources after added to index

---
+++ app/src/main/java/sk/inloop/support/sample/adapter/IncorrectFragmentAdapter.java
@@ -0,0 +1,62 @@
+package sk.inloop.support.sample.adapter;
+
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentStatePagerAdapter;
+
+import sk.inloop.support.sample.SampleFragment;
+
+/**
+ * Extends {@link FragmentStatePagerAdapter} that fails on {@link #notifyDataSetChanged()}.
+ */
+
+public class IncorrectFragmentAdapter extends FragmentStatePagerAdapter {
+    private boolean mState = true;
+
+    public IncorrectFragmentAdapter(FragmentManager fragmentManager) {
+        super(fragmentManager);
+    }
+
+    @Override
+    public int getCount() {
+        return 3;
+    }
+
+    public void toggleState() {
+        mState = !mState;
+        notifyDataSetChanged();
+    }
+
+    private String getLabel(int position) {
+        switch (position) {
+            case 0:
+                return "A";
+            case 1:
+                return mState ? "B" : "C";
+            default:
+                return mState ? "C" : "B";
+        }
+    }
+
+    @Override
+    public int getItemPosition(Object object) {
+        String label = ((SampleFragment) object).getLabel();
+        if (label.equals("A")) {
+            return 0;
+        } else if (label.equals("B")) {
+            return mState ? 1 : 2;
+        } else {
+            return mState ? 2 : 1;
+        }
+    }
+
+    @Override
+    public CharSequence getPageTitle(int position) {
+        return getLabel(position);
+    }
+
+    @Override
+    public Fragment getItem(int position) {
+        return SampleFragment.newInstance(getLabel(position));
+    }
+}
