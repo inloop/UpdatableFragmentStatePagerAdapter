62c32258b4434d09c8b66b7eff42de021c02c699
Daniel Novak <novak@inloop.eu>
2016-12-12 13:09:24 +0100

    Basic implementation

--- sample/src/main/java/eu/inloop/pager/sample/MainActivity.java
+++ sample/src/main/java/eu/inloop/pager/sample/MainActivity.java
@@ -14,20 +14,24 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
+import eu.inloop.pager.UpdatableFragmentPagerAdapter;
+
 public class MainActivity extends AppCompatActivity {
 
-    private static List<Integer> sDataSet;
+    private ArrayList<Integer> sDataSet;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        if (sDataSet == null) {
+        if (savedInstanceState == null) {
             sDataSet = new ArrayList<>();
             for (int i = 0; i < 6; i++) {
                 sDataSet.add(i);
             }
+        } else {
+            sDataSet = savedInstanceState.getIntegerArrayList("dataset");
         }
 
         final ViewPager viewPager = (ViewPager) findViewById(R.id.pager);
@@ -41,9 +45,25 @@ public class MainActivity extends AppCompatActivity {
                 adapter.notifyDataSetChanged();
             }
         });
+        findViewById(R.id.btn_add).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                sDataSet.add(sDataSet.size());
+                adapter.notifyDataSetChanged();
+            }
+        });
+        findViewById(R.id.btn_removeitem).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (sDataSet.size() > 0) {
+                    sDataSet.remove(sDataSet.size() - 1);
+                    adapter.notifyDataSetChanged();
+                }
+            }
+        });
     }
 
-    private static class PagerAdapter extends FragmentStatePagerAdapter {
+    private static class PagerAdapter extends UpdatableFragmentPagerAdapter {
 
         private final List<Integer> mItems;
 
@@ -63,16 +83,26 @@ public class MainActivity extends AppCompatActivity {
         }
 
         @Override
+        public long getItemId(int position) {
+            return mItems.get(position);
+        }
+
+        @Override
         public int getItemPosition(Object object) {
             PageFragment item = (PageFragment) object;
             int itemValue = item.getSomeIdentifier();
             for (int i = 0; i < mItems.size(); i++) {
                 if (mItems.get(i).equals(itemValue)) {
-                    int position = i;
-                    return position;
+                    return i;
                 }
             }
             return POSITION_NONE;
         }
+    }
+
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        outState.putIntegerArrayList("dataset", sDataSet);
     }
 }
